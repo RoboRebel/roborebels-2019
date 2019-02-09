562d00104a28a855e6c20e783ff79572cfc62445
Hexbugman213 <minecrafthex213@gmail.com>
2019-02-09 12:04:35 -0600

    placeholder code for hatch grabbing

--- src/main/java/frc/robot/commands/HatchDropoff.java
+++ src/main/java/frc/robot/commands/HatchDropoff.java
@@ -1,10 +1,21 @@
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class HatchDropoff extends Command {
+public class HatchDropoff extends CommandGroup {
     @Override
     protected boolean isFinished() {
         return false;
+    }
+    @Override
+    protected void initialize() {
+    }
+    @Override
+    protected void execute() {
+        addSequential(new ExtendActuator1());
+        addSequential(new ContractActuator2());
+        addSequential(new ContractActuator1());
     }
 }
--- src/main/java/frc/robot/commands/HatchPickup.java
+++ src/main/java/frc/robot/commands/HatchPickup.java
@@ -1,11 +1,21 @@
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class HatchPickup extends Command {
-
+public class HatchPickup extends CommandGroup {
     @Override
     protected boolean isFinished() {
         return false;
+    }
+    @Override
+    protected void initialize() {
+    }
+    @Override
+    protected void execute() {
+        addSequential(new ExtendActuator1());
+        addSequential(new ExtendActuator2());
+        addSequential(new ContractActuator1());
     }
 }
