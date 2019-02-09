08be6109e6cfae1cbd58023c2a24242577dcd2ce
Hexbugman213 <minecrafthex213@gmail.com>
2019-02-09 12:42:04 -0600

    ball

--- src/main/java/frc/robot/commands/BallDropoff.java
+++ src/main/java/frc/robot/commands/BallDropoff.java
@@ -1,16 +1,28 @@
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 
+import javax.management.timer.Timer;
+
 public class BallDropoff extends Command {
-
-    public BallDropoff(){
-        requires(Robot.shooter);
+    public BallDropoff() {
+        this.requires(Robot.shooter);
     }
-
     @Override
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
+    }
+    @Override
+    protected void initialize() {
+        setTimeout(1000);
+    }
+    @Override
+    protected void execute() {
+        Robot.shooter.set(1.0);
+    }
+    @Override
+    protected void end() {
+        Robot.shooter.set(0);
     }
 }
